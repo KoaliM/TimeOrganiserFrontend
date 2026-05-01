8673e41a43859821016fcd8aa8c382483e103ee1
KoaliM <mihaelahristova071@gmail.com>
2026-05-01 14:41:05 +0300

    adding api services, users pinia store, inerfaces in type.ts

--- time-organiser/src/components/Navbar.vue
+++ time-organiser/src/components/Navbar.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
+import {ref} from 'vue'
 </script>
 
 <template>
+    <div class = "flex items-center object-top rounded-xl " >
+        <RouterLink to="/">Befriend</RouterLink>
+        <RouterLink to="/">Meet</RouterLink>
+        <RouterLink to="/">My tasks</RouterLink>
+        <RouterLink to="/" v-if="">Login/Sign up</RouterLink>
+    </div>
 </template>
---
+++ time-organiser/src/stores/users.ts
@@ -0,0 +1,37 @@
+import type { User } from '@/utils/types';
+import {defineStore} from 'pinia'
+import {ref,computed} from 'vue'
+
+export const useUserStore = defineStore('user', {
+    state: ()=> ({
+        profile: null as User | null,
+        accessToken: null as string | null,
+        refreshToken: localStorage.getItem('refreshToken')
+    }),
+    getters: {
+        isAuthenticated: state => !! state.accessToken
+        //isAdmin: state => {return state.profile?.role.valueOf("Admin")},
+    },
+    actions: {
+        setAccessToken(token: string) {
+			this.accessToken = token;
+		},
+		setRefreshToken(token: string) {
+			this.refreshToken = token;
+
+			localStorage.setItem("refreshToken", token);
+		},
+		async logout() {
+			this.accessToken = null;
+			this.profile = null;
+			this.refreshToken = null;
+			localStorage.removeItem("refreshToken");
+		},
+        async clearUserData(){
+            this.accessToken=null;
+            this.profile = null;
+            this.refreshToken = null;
+            localStorage.removeItem("refreshToken");
+        }
+    }
+})
